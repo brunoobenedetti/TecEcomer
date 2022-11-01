a675ce5faab98bcdedab22adc835a23b3065e765
brunoobenedetti <bbenedetti854@gmail.com>
2022-11-01 13:31:44 -0300

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # tececomer
 # tececomer
 # tececomer
+# tececomer
---
+++ js/stock.js
@@ -0,0 +1,29 @@
+const productos = [
+    {id: 1, nombre: "pc", tipo: "GAMER", desc: "Una PC GAMER para que la uses al maximo", precio: 1200, marca: "ASUS", img: '/img/pc-gamer.jpg'},
+    {id: 2, nombre: "pc", tipo: "GAMER", desc: "Una PC GAMER para que la uses al maximo", precio: 1100, marca: "ASUS", img: '/img/pc-gamer.jpg'},
+    {id: 3, nombre: "pc", tipo: "GAMER", desc: "Una PC GAMER para que la uses al maximo", precio: 1200, marca: "ASUS", img: '/img/pc-gamer.jpg'},
+    {id: 4, nombre: "pc", tipo: "GAMER", desc: "Una PC GAMER para que la uses al maximo", precio: 1400, marca: "ASUS", img: '/img/PC_gamer1.jpg'},
+    {id: 5, nombre: "pc", tipo: "GAMER", desc: "Una PC GAMER para que la uses al maximo", precio: 1200, marca: "ASUS", img: '/img/PC_gamer1.jpg'},
+    {id: 6, nombre: "pc", tipo: "GAMER", desc: "Una PC GAMER para que la uses al maximo", precio: 1500, marca: "ASUS", img: '/img/PC_gamer1.jpg'},
+    {id: 7, nombre: "NOTEBOOK ", tipo: "GAMER PORTATIL", desc: "PARA que puedas jugar en donde quieras ", precio: 500, marca:"LENOVO Legion", img:'/img/notebookgamer.jpg'},
+    {id: 8, nombre: "NOTEBOOK ", tipo:"GAMER PORTATIL", desc: "PARA que puedas jugar en donde quieras", precio: 500, marca: "LENOVO Legion", img: '/img/notebookgamer.jpg'},
+    {id: 9, nombre: "NOTEBOOK ", tipo:"GAMER PORTATIL", desc: "PARA que puedas jugar en donde quieras", precio: 500, marca: "LENOVO Legion", img: '/img/notebookgamer.jpg'},
+    {id: 10, nombre:"NOTEBOOK ", tipo:"GAMER PORTATIL", desc: "PARA que puedas jugar en donde quieras", precio: 700, marca: "LENOVO Legion", img: '/img/notebook1.jpg'},
+    {id: 11, nombre:"NOTEBOOK ", tipo:"GAMER PORTATIL", desc: "PARA que puedas jugar en donde quieras", precio: 700, marca: "LENOVO Legion", img: '/img/notebook1.jpg'},
+    {id: 12, nombre:"NOTEBOOK ", tipo:"GAMER PORTATIL", desc: "PARA que puedas jugar en donde quieras", precio: 700, marca: "LENOVO Legion", img: '/img/notebook1.jpg'},
+    {id: 13, nombre: "MONITOR GAMER ", tipo: "GAMER ulta 8k", desc: "para que jueges tus juego con alat definicion ", precio: 700, marca: "samsung", img: '/img/monitor_gamer.jpg'},
+    {id: 14, nombre: "MONITOR GAMER ", tipo: "GAMER ulta 8k", desc: "para que jueges tus juego con alat definicion ", precio: 700, marca: "samsung", img: '/img/monitor_gamer.jpg'},
+    {id: 15, nombre: "MONITOR GAMER ", tipo: "GAMER ulta 8k", desc: "para que jueges tus juego con alat definicion ", precio: 700, marca: "samsung", img: '/img/monitor_gamer.jpg'},
+    {id: 16, nombre: "MONITOR GAMER ", tipo: "GAMER ulta 8k", desc: "para que jueges tus juego con alat definicion ", precio: 900, marca: "samsung", img: '/img/monitor1.jpg'},
+    {id: 17, nombre: "MONITOR GAMER ", tipo: "GAMER ulta 8k", desc: "para que jueges tus juego con alat definicion ", precio: 1400, marca:"samsung", img: '/img/monitor1.jpg'},
+    {id: 18, nombre: "MONITOR GAMER ", tipo: "GAMER ulta 8k", desc: "para que jueges tus juego con alat definicion ", precio: 7000, marca:"samsung", img: '/img/monitor1.jpg'},
+    {id: 19, nombre: "TECLADO ", tipo: "GAMER ", desc: "PARA QUE TENGAS MAS COMODIDAD A LA HORA DE JUGAR ", precio: 777, marca: "redragon", img: '/img/tecladogamer.jpg'},
+    {id: 20, nombre: "TECLADO ", tipo: "GAMER ", desc: "PARA QUE TENGAS MAS COMODIDAD A LA HORA DE JUGAR ", precio: 234, marca: "redragon", img: '/img/tecladogamer.jpg'},
+    {id: 21, nombre: "TECLADO ", tipo: "GAMER ", desc: "PARA QUE TENGAS MAS COMODIDAD A LA HORA DE JUGAR ", precio: 1600, marca:"redragon", img: '/img/tecladogamer.jpg'},
+    {id: 22, nombre: "TECLADO ", tipo: "GAMER ", desc: "PARA QUE TENGAS MAS COMODIDAD A LA HORA DE JUGAR ", precio: 1600, marca:"redragon", img: '/img/teclado1.jpg'},
+    {id: 23, nombre: "TECLADO ", tipo: "GAMER ", desc: "PARA QUE TENGAS MAS COMODIDAD A LA HORA DE JUGAR ", precio: 3200, marca:"redragon", img: '/img/teclado1.jpg'},
+    {id: 24, nombre: "TECLADO ", tipo: "GAMER", desc: "PARA QUE TENGAS MAS COMODIDAD A LA HORA DE JUGAR ", precio: 2300, marca: "redragon", img: '/img/teclado1.jpg'},
+]
+
+
+
